404ac854ed8af43429b233e6f6f3667cdd0e1e6e
Elijahtum <kiprutoelijah@gmail.com>
2022-02-17 01:22:56 +0300

    update for Html and Java Script

--- aseets/js/main.js
+++ aseets/js/main.js
@@ -1,1 +1,18 @@
 document.getElementById("intro_Message").innerHTML = `Best Online Shop! Order Now And we deliver to your Door!`;
+
+//Class creation calculating Current Year
+class introduction {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+    age() {
+      let date = new Date();
+      return date.getFullYear() - this.year;
+    }
+  }
+  
+  let myYear = new introduction("January", 2018);
+  document.getElementById("intro").innerHTML =
+  "The Shop is " + myYear.age() + " years old.";
+
