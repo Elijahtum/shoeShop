312c749473ce7a956550980ba095bb36a691d756
Elijahtum <kiprutoelijah@gmail.com>
2022-02-20 22:47:29 +0300

    Update in js

--- aseets/js/main.js
+++ aseets/js/main.js
@@ -16,3 +16,52 @@ class introduction {
   document.getElementById("intro").innerHTML =
   "The Shop is " + myYear.age() + " years old.";
 
+// Date Checker Function
+function displayDate() {
+    document.getElementById("Time_trial").innerHTML = Date();
+  };
+
+//Object Method
+const person = {
+    firstName:"Founded By Eli.",
+    lastName:"kokwet Business Empire",
+    id: 1016,
+    fullName: function (){
+        return this.firstName + "    " + this.lastName;
+    }
+}
+document.getElementById("Person").innerHTML = person.fullName();
+
+//
+
+/*
+// Define an object
+const obj = {counter : 0};
+
+// Play with counter:
+obj.reset;
+obj.add = 10;
+obj.subtract = 1;
+obj.increment;
+obj.decrement;
+
+// Define Setters and Getters
+Object.defineProperty(obj, "reset", {
+  get : function () {this.counter = 0;}
+});
+Object.defineProperty(obj, "increment", {
+  get : function () {this.counter++;}
+});
+Object.defineProperty(obj, "decrement", {
+  get : function () {this.counter--;}
+});
+Object.defineProperty(obj, "add", {
+  set : function (value) {this.counter += value;}
+});
+Object.defineProperty(obj, "subtract", {
+  set : function (value) {this.counter -= value;}
+});
+
+document.getElementById("counter_Demo").innerHTML = obj.counter;
+*/
+
